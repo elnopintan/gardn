005807850f67a31fa21709115a87af0d9604e3f9
Ignacio Blasco Lopez <ibl@tid.es>
2013-03-08 14:06:32 +0100

    first commit

---
+++ src/gardn/core.clj
@@ -0,0 +1,39 @@
+(ns gardn.core)
+
+;Gardn Id. 
+;reference identifies a gardn entity.
+;instance refer to a given state of the entity.
+(defrecord Id 
+  [reference instance])
+
+;Gardn data. Represent a snapshot of a gardn entity. 
+;origin is the Id of the previous state of the entity.
+(defrecord Data 
+  [origin id value])
+
+(defn new-entity 
+  "Creates a new entity with a given reference and value"
+  [reference value]
+  (Data. nil (Id. reference (.hashCode value)) value))
+
+
+(defn update-entity 
+  "Updates an entity to a new value which is created applying a given function"
+  [origin f]
+  (let [{:keys [id value]} origin
+        {:keys [reference instance]} id
+        new-value (f value)]
+    (Data. id (Id. reference (.hashCode new-value)) new-value)))
+
+(def nacho (new-entity "Nacho" "Hola "))
+
+(update-entity nacho #(.trim %))
+(defprotocol Store
+  (get-entity
+   "Retrieves a given entity by it's Id. 
+   If instance is :last retrieva the last entity"
+   [id])
+  (persist! 
+   "Persists a given gardn entity. Returns true if it's persisted and false 
+   if there is already an entity with the same origin."
+   [entity]))
---
+++ test/gardn/core_test.clj
@@ -0,0 +1,7 @@
+(ns gardn.core-test
+  (:use clojure.test
+        gardn.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
